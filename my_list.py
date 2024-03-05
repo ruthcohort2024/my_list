2a62d0b96c478a4d3517b66471379403e8f0b6c3
ruthcohort2024 <ruthkariuki586@gmail.com>
2024-03-05 23:15:57 +0300

    first commit

---
+++ my_list.py
@@ -0,0 +1,15 @@
+List_a = [10, 20, 30, 40]
+#index     0   1   2   3
+print(List_a)
+List_a[1] = 15
+print(List_a)
+List_a.extend([50, 60, 70])
+print(List_a)
+List_a.pop(6)
+print(List_a)
+
+for item in List_a:
+    print(item)
+
+
+    
